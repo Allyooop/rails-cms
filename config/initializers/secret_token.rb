30407a984cacc5b351baa1e7ebb146065001aa91
GrilloPress <grillopress@gmail.com>
2015-01-29 18:23:31 +0000

    initial creation of rails loco

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teethree::Application.config.secret_token = '7ae37b1abb8e345a4d576ceaf4daea7cd5e997198bda0ea69d2ae9ee7d121aede5d1f16e59fb6fdc6a85617069864eba9252b7282728073e4062df9b5d6da74c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Teethree::Application.config.session_store :cookie_store, key: '_teethree_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Teethree::Application.config.session_store :active_record_store
